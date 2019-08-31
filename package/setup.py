7b383e0812e7cec136f9e227c1100e07e227509d
Sabine <sabine.bertram@mailbox.org>
2019-08-31 15:17:34 -0400

    1.0.0

--- package/setup.py
+++ package/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='studentNFT_API',
-   version='0.1.0',
+   version='1.0.0',
    maintainer='Sabine Bertram',
    maintainer_email='sabine.bertram@mailbox.org',
    package_dir={'': 'src'},
