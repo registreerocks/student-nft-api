7d56e3cf2cd95563ed16485bd21c2c49cf157902
Sabine <sabine.bertram@mailbox.org>
2020-04-06 17:55:25 +0200

    test: add test

--- package/src/swagger_server/controllers/keys.py
+++ package/src/swagger_server/controllers/keys.py
@@ -1,4 +1,4 @@
 from os import environ as env
 
-infura_key = env.get("INFURA_KEY")
-private_key = env.get("PRIVATE_KEY")
+infura_key = env.get("INFURA_KEY", 'somekey')
+private_key = env.get("PRIVATE_KEY", '1f7ca3635605bc57b579b5fdf73e3ac84aa91a9c8eddc57aa0e3452d1a429ff8')
