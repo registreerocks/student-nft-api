570abef806899ed84bb0f8c65dfa93781f97cd29
Sabine <sabine.bertram@mailbox.org>
2020-12-11 10:17:21 +0100

    feat: remove blockchain components
    * only keeps web3 hashing algorithm
    * refactors get identifying id to use db lookup and not recalculating id
    * refactors bulk get identifying ids to not use multiple db lookups but only one find all

--- package/src/swagger_server/controllers/contracts.py
+++
@@ -1,6 +0,0 @@
-import requests
-import json
-
-response = requests.get("https://raw.githubusercontent.com/registreerocks/studentNFT/master/build/contracts/Registree.json")
-registree_interface = response.json()
-registree_address = registree_interface['networks']['4']['address']
--- package/src/swagger_server/controllers/global_vars.py
+++ package/src/swagger_server/controllers/global_vars.py
@@ -9,6 +9,3 @@ CLIENT = MongoClient(
   )
 DB = CLIENT.database
 JOIN = DB.joining_db
-
-INFURA_KEY = env.get("INFURA_KEY", 'somekey')
-PRIVATE_KEY = env.get("PRIVATE_KEY", '1f7ca3635605bc57b579b5fdf73e3ac84aa91a9c8eddc57aa0e3452d1a429ff8')
